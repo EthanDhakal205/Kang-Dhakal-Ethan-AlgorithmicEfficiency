e552d41f13a3265ac631485d35987409e0ebd661
EthanDhakal205 <ethan.rajdhakal@gmail.com>
2026-04-17 13:47:02 -0700

    Add MergeSort.java

---
+++ Searching/MergeSort.java
@@ -0,0 +1,9 @@
+public final class MergeSort {
+
+    private MergeSort() {
+    }
+
+    public static SortEngine.SortRun run(int[] input) {
+        return SortEngine.run("Merge Sort", input);
+    }
+}
