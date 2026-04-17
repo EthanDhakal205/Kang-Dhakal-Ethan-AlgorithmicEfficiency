9ccaa596932f545efc0ca0deb06b7d65bd623c72
EthanDhakal205 <ethan.rajdhakal@gmail.com>
2026-04-17 13:47:01 -0700

    Add BubbleSort.java

---
+++ Searching/BubbleSort.java
@@ -0,0 +1,9 @@
+public final class BubbleSort {
+
+    private BubbleSort() {
+    }
+
+    public static SortEngine.SortRun run(int[] input) {
+        return SortEngine.run("Bubble Sort", input);
+    }
+}
