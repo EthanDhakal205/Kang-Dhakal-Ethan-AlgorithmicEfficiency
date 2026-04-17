5f4e63b9d1fe9480050f7460f69ec671ee5a9e18
EthanDhakal205 <ethan.rajdhakal@gmail.com>
2026-04-17 13:47:01 -0700

    Add HeapSort.java

---
+++ Searching/HeapSort.java
@@ -0,0 +1,9 @@
+public final class HeapSort {
+
+    private HeapSort() {
+    }
+
+    public static SortEngine.SortRun run(int[] input) {
+        return SortEngine.run("Heap Sort", input);
+    }
+}
