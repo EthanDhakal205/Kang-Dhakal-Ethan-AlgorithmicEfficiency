2bf5065eca29c23b474e792d2b3bc3db6b7c6f7d
EthanDhakal205 <ethan.rajdhakal@gmail.com>
2026-04-17 13:47:04 -0700

    Add TimSort.java

---
+++ Searching/TimSort.java
@@ -0,0 +1,9 @@
+public final class TimSort {
+
+    private TimSort() {
+    }
+
+    public static SortEngine.SortRun run(int[] input) {
+        return SortEngine.run("Tim Sort", input);
+    }
+}
