4babe9fd6fd36f0d8a0d58736d8240db712a26b2
EthanDhakal205 <ethan.rajdhakal@gmail.com>
2026-04-17 13:47:02 -0700

    Add InsertionSort.java

---
+++ Searching/InsertionSort.java
@@ -0,0 +1,9 @@
+public final class InsertionSort {
+
+    private InsertionSort() {
+    }
+
+    public static SortEngine.SortRun run(int[] input) {
+        return SortEngine.run("Insertion Sort", input);
+    }
+}
