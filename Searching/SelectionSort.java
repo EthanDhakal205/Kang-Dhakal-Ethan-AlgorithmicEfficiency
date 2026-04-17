dca5cdb9a1000477dd94c1e48b7c79598504ceb4
EthanDhakal205 <ethan.rajdhakal@gmail.com>
2026-04-17 13:47:04 -0700

    Add SelectionSort.java

---
+++ Searching/SelectionSort.java
@@ -0,0 +1,9 @@
+public final class SelectionSort {
+
+    private SelectionSort() {
+    }
+
+    public static SortEngine.SortRun run(int[] input) {
+        return SortEngine.run("Selection Sort", input);
+    }
+}
