cb836fee15942dad0b9a95615de60d290dea0c60
EthanDhakal205 <ethan.rajdhakal@gmail.com>
2026-04-17 13:47:03 -0700

    Add QuickSort.java

---
+++ Searching/QuickSort.java
@@ -0,0 +1,9 @@
+public final class QuickSort {
+
+    private QuickSort() {
+    }
+
+    public static SortEngine.SortRun run(int[] input) {
+        return SortEngine.run("Quick Sort", input);
+    }
+}
