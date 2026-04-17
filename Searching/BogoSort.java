7baeeb1946d139ea3b64875d7d89d418f1492e45
EthanDhakal205 <ethan.rajdhakal@gmail.com>
2026-04-17 13:47:01 -0700

    Add BogoSort.java

---
+++ Searching/BogoSort.java
@@ -0,0 +1,9 @@
+public final class BogoSort {
+
+    private BogoSort() {
+    }
+
+    public static SortEngine.SortRun run(int[] input) {
+        return SortEngine.run("Bogo Sort", input);
+    }
+}
